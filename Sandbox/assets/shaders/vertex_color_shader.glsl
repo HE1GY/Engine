e20419000def1815bb484cc3eecac89cc6613839
Oleh <oleglavro6@gmail.com>
2023-01-30 19:16:01 +0200

    start 3D

--- Sandbox/assets/shaders/vertex_color_shader.glsl
+++ Sandbox/assets/shaders/vertex_color_shader.glsl
@@ -1,20 +1,24 @@
 #type vertex
 #version 460 core
 layout(location=0)in vec3 a_position;
+layout(location=1)in vec4 a_color;
 uniform mat4 u_view_project;
 uniform mat4 u_model;
 
-out vec3 v_color;
+out vec4 v_color;
 void main()
 {
+    v_color=a_color;
     gl_Position=u_view_project*u_model*vec4(a_position, 1);
 }
 
 #type fragment
 #version 460 core
-layout(location=0)out vec4 color;
-uniform vec4 u_color;
+in vec4 v_color;
+
+out vec4 color;
+
 void main()
 {
-    color=u_color;
+    color=v_color;
 }
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -12,7 +12,8 @@ public:
 			:Layer("ExampleLayer"), m_camera_controller(1280.0f / 720.0f, true)
 	{
 		m_shader_lib.Load("texture", "../../../Sandbox/assets/shaders/default_2D_shader.glsl");
-		m_shader_lib.Load("flat_color", "../../../Sandbox/assets/shaders/uniform_color_shader.glsl");
+
+		auto shader = m_shader_lib.Load("vertex_color", "../../../Sandbox/assets/shaders/vertex_color_shader.glsl");
 
 		m_texture_2 = Engine::Texture2D::Create("../../../Sandbox/assets/textures/ILTQq.png");
 		m_texture = Engine::Texture2D::Create("../../../Sandbox/assets/textures/wallpaperflare.com_wallpaper.png");
@@ -44,27 +45,45 @@ public:
 		}
 		m_vao_square_texture2d->UnBind();
 
-		m_vao_square = Engine::VertexArray::Create();
-		m_vao_square->Bind();
+		m_vao_box = Engine::VertexArray::Create();
+		m_vao_box->Bind();
 		{
 			float pos_box[] = {
-					-0.5, -0.5, 0, 0.5, -0.5, 0, 0.5, 0.5, 0, -0.5, 0.5, 0,
+					-0.5, -0.5, 0, 0.3f, 0, 0, 1,//0
+					0.5, -0.5, 0, 0.6f, 0, 0, 1,//1
+					0.5, 0.5, 0, 0.9f, 0, 0, 1,//2
+					-0.5, 0.5, 0, 0, 0.3f, 0, 1,//3
+
+					-0.5, -0.5, -1, 0, 0.6f, 0, 1,//4
+					0.5, -0.5, -1, 0, 0.9f, 0, 1,//5
+					0.5, 0.5, -1, 0, 0, 0.3f, 1,//6
+					-0.5, 0.5, -1, 0., 0, 0.6f, 1,//7
 			};
 			Engine::Ref<Engine::VertexBuffer> vo_data_box;
 			vo_data_box = Engine::VertexBuffer::Create(pos_box, sizeof(pos_box));
 			vo_data_box->Bind();
 
-			vo_data_box->set_layout({{ "a_position", Engine::ShaderDataType::Float3, false }});
+			vo_data_box->set_layout({
+					{ "a_position", Engine::ShaderDataType::Float3, false },
+					{ "a_color", Engine::ShaderDataType::Float4, false }
+			});
 
-			uint32_t elements_box[] = { 0, 1, 2, 2, 3, 0 };
+			uint32_t elements_box[] = {
+					0, 1, 2, 2, 3, 0,
+					4, 5, 6, 6, 7, 4,
+					3, 2, 6, 6, 7, 3,
+					0, 1, 5, 5, 4, 0,
+					1, 5, 6, 6, 2, 1,
+					0, 4, 7, 7, 3, 0,
+			};
 			Engine::Ref<Engine::IndexBuffer> vo_index_box;
 			vo_index_box = Engine::IndexBuffer::Create(elements_box, sizeof(elements_box) / sizeof(uint32_t));
 			vo_index_box->Bind();
 
-			m_vao_square->AddVertexBuffer(vo_data_box);
-			m_vao_square->SetIndexBuffer(vo_index_box);
+			m_vao_box->AddVertexBuffer(vo_data_box);
+			m_vao_box->SetIndexBuffer(vo_index_box);
 		}
-		m_vao_square->UnBind();
+		m_vao_box->UnBind();
 	}
 
 	void OnEvent(Engine::Event& event) override
@@ -81,7 +100,7 @@ public:
 
 		Engine::Renderer::BeginScene(m_camera_controller.get_camera());
 
-		float x{ 0 };
+		/*float x{ 0 };
 		float y{ 0 };
 		glm::vec3 square_pos{ 0, 0, 0 };
 		for (int i = 0; i < 20; ++i)
@@ -94,31 +113,28 @@ public:
 				auto flat_shader = m_shader_lib.Get("flat_color");
 				flat_shader->Bind();
 				flat_shader->SetVec4("u_color", m_uniform_color);
-				Engine::Renderer::Submit(flat_shader, m_vao_square,
+				Engine::Renderer::Submit(flat_shader, m_vao_box,
 						glm::translate(glm::mat4(1.0f), square_pos) *
 								glm::scale(glm::mat4(1.0f), { 0.1f, 0.1f, 0.1f }));
 			}
-		}
+		}*/
 
-		m_texture->Bind();
-		auto texture = m_shader_lib.Get("texture");
-		texture->Bind();
-		texture->SetInt("u_texture2D", 0);
-		Engine::Renderer::Submit(texture, m_vao_square_texture2d);
 
-		m_texture_2->Bind();
-		texture->Bind();
-		texture->SetInt("u_texture2D", 0);
-		Engine::Renderer::Submit(texture, m_vao_square_texture2d);
+		Engine::Ref<Engine::Shader> shader = m_shader_lib.Get("vertex_color");
+		shader->Bind();
+
+		glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), { 0, 0, 1 })
+				* glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), { 0, 1, 0 });
+		Engine::Renderer::Submit(shader, m_vao_box, rotation);
 
 		Engine::Renderer::EndScene();
 	}
 
 	void OnImGuiRender()
 	{
-		ImGui::ColorEdit4("color", &m_uniform_color.r);
+		/*ImGui::ColorEdit4("color", &m_uniform_color.r);
 		ImVec2 size = { 500, 100 };
-		ImGui::SetWindowSize(size);
+		ImGui::SetWindowSize(size);*/
 	}
 
 private:
@@ -126,7 +142,7 @@ private:
 
 	Engine::Ref<Engine::VertexArray> m_vao_square_texture2d;
 
-	Engine::Ref<Engine::VertexArray> m_vao_square;
+	Engine::Ref<Engine::VertexArray> m_vao_box;
 
 	Engine::Ref<Engine::Texture2D> m_texture;
 	Engine::Ref<Engine::Texture2D> m_texture_2;
@@ -141,8 +157,8 @@ class SandBox : public Engine::Application
 public:
 	SandBox()
 	{
-		//PushLayer(new ExampleLayer());
-		PushLayer(new Sandbox2D());
+		PushLayer(new ExampleLayer());
+		//PushLayer(new Sandbox2D());
 	}
 
 	~SandBox() override = default;
